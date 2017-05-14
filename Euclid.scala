74880a91cc23e1c6aa6bf07d44567e25f32ea767
TosakaZ <amenbows@yahoo.co.jp>
2017-05-14 21:23:45 +0900

    【Scala基礎コース03 挑戦】ユークリッド互除法をつかって最大公約数を求めるオブジェクトを実装

--- Euclid.scala
+++ Euclid.scala
@@ -1,7 +1,22 @@
 object Euclid extends App {
 
   def greatestCommonDivisor(a: Int, b: Int): Int = {
-    ???
+    if (a == 0 || b == 0) {
+      println("最大公約数なし")
+      return a + b
+    } else if (a == b) {
+      a
+    } else if (a > b) {
+      val c = a - b
+      if (c >= b) {
+        greatestCommonDivisor(c, b)
+      } else {
+        greatestCommonDivisor(b, c)
+      }
+    } else {
+      greatestCommonDivisor(b, a)
+    }
   }
-
+//  println(greatestCommonDivisor(1649, 221))
+//  println(greatestCommonDivisor(0, 221))
 }
