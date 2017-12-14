c91fe1fc5de3040d7427d93b61310da014875508
s1lmer1a <you@sample.com>
2017-12-14 16:25:11 +0000

    Implement Euclid.greatestCommonDivisor

--- Euclid.scala
+++ Euclid.scala
@@ -1,7 +1,14 @@
 object Euclid extends App {
 
-  def greatestCommonDivisor(a: Int, b: Int): Int = {
-    ???
+  def greatestCommonDivisor(big_num: Int, small_num: Int): Int = {
+    if (big_num == 0) {
+      small_num
+    } else if (small_num == 0) {
+      big_num
+    } else {
+      var remainder = big_num % small_num
+      greatestCommonDivisor(small_num, remainder)
+    }
   }
 
 }
