d469adc4dbd597d8ef9be2f8ff08fbb84b28ea97
小屋松裕貴 <yukikoyamatsu@koyamatsuyukinoMacBook-Pro.local>
2018-05-20 16:35:23 +0900

    ユークリッド互除法の実装

--- Euclid.scala
+++ Euclid.scala
@@ -1,7 +1,15 @@
 object Euclid extends App {
 
   def greatestCommonDivisor(a: Int, b: Int): Int = {
-    ???
+    if(a <= b){
+      return greatestCommonDivisor(b,a)
+    }
+    if(a == 0){
+        return a
+    }else if (b == 0){
+        return a
+    }else{
+      return greatestCommonDivisor(b,a % b)
+    }
   }
-
 }
