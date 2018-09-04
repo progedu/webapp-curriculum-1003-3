581b6332daa80a1bb2e3b5d86a5c65481b558502
noratora <4438333+noratora@users.noreply.github.com>
2018-09-05 01:25:19 +0900

    ユークリッド互除法をつかって最大公約数を求める処理を実装

--- Euclid.scala
+++ Euclid.scala
@@ -1,7 +1,10 @@
 object Euclid extends App {
 
   def greatestCommonDivisor(a: Int, b: Int): Int = {
-    ???
+    // a と b が 0 の場合
+    if (a == 0) return b
+    // 割り切れた場合
+    if (b == 0) return a
+    return greatestCommonDivisor(b, a % b)
   }
-
 }
