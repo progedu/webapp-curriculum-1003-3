bd2c25f63c63e0c0a0a9449344d6ce4ee7be5b03
208-209 <208_209@outlook.jp>
2018-11-25 19:27:30 +0900

    最大公約数を求めるメソッドの実装

--- Euclid.scala
+++ Euclid.scala
@@ -1,7 +1,9 @@
 object Euclid extends App {
 
-  def greatestCommonDivisor(a: Int, b: Int): Int = {
-    ???
+  def greatestCommonDivisor(a: Int, b: Int): Int = (a, b) match {
+    case (0, _) => b
+    case (_, 0) => a
+    case (_, _) => greatestCommonDivisor(b, a % b)
   }
 
 }
