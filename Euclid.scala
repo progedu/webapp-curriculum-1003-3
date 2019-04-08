3621bb0acbeaf7f42c1e0c7ed332f1df6c5e1761
syo <M.youta.kwg@gmail.com>
2019-04-08 22:59:04 +0900

    ユークリッドの互除法を実装

--- Euclid.scala
+++ Euclid.scala
@@ -1,7 +1,11 @@
 object Euclid extends App {
+  println(greatestCommonDivisor(0, 0) == 0)
+  println(greatestCommonDivisor(12, 0) == 12)
+  println(greatestCommonDivisor(12, 18) == 6)
 
-  def greatestCommonDivisor(a: Int, b: Int): Int = {
-    ???
+  def greatestCommonDivisor(a: Int, b: Int): Int = (a, b) match {
+    case (0, _) => b
+    case (_, 0) => a
+    case (a, b) => greatestCommonDivisor(b, a % b)
   }
-
 }
