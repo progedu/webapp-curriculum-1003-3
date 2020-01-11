d14a671586e769988d47a71ea8fc2104a366a363
近藤康介 <kosuke@k-kosukeMacBook.local>
2020-01-11 23:56:36 +0900

    euclid_fix

--- Euclid.scala
+++ Euclid.scala
@@ -1,11 +1,12 @@
 object Euclid extends App {
 
   def greatestCommonDivisor(a: Int, b: Int): Int = {
-    if (b == 0) {
+    if (a == 0) {
+      b
+    } else if (b == 0) {
       a
-    } else {
-      greatestCommonDivisor(b, a % b)
-    }
+    }else
+    greatestCommonDivisor(b, a % b)
   }
 
   println(greatestCommonDivisor(36,24))
