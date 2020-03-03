b8cd7dca6373a4b63ddec2a21f7cf7dcc8b9ac40
緑川俊輔 <blackcalicocat@midorikawashunsukenoMacBook-Pro.local>
2020-03-03 16:50:07 +0900

    GCDを求める関数の実装

--- Euclid.scala
+++ Euclid.scala
@@ -1,7 +1,8 @@
-object Euclid extends App {
-
-  def greatestCommonDivisor(a: Int, b: Int): Int = {
-    ???
-  }
-
+object Euclid extends App{
+    def greatestCommonDivisor(a:Int,b:Int):Int={
+        if(a==0) b;
+        else if(b==0) a;
+        else if(a<b)greatestCommonDivisor(b,a)
+        else greatestCommonDivisor(b,a%b)
+    }
 }
