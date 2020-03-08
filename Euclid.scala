70f9d46bf25f938105cc26239edf92ded9b52565
Ryusuke Iida <iida-ryusuke469@g.ecc.u-tokyo.ac.jp>
2020-03-08 09:22:01 +0900

    ユークリッドの互除法を実装

--- Euclid.scala
+++ Euclid.scala
@@ -2,12 +2,12 @@ object Euclid extends App {
 
   def greatestCommonDivisor(a: Int, b: Int): Int = {
     if (a == 0) {
-	  b
-	} else if (b == 0) {
-	  a
-	} else {
-	  greatestCommonDivisor(b, a % b)
-	}
+      b
+    } else if (b == 0) {
+      a
+    } else {
+      greatestCommonDivisor(b, a % b)
+    }
   }
 
 }
