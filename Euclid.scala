85d0d95713053d6bd7c81855c48ec7f2c70d375f
m-tajima <onioni0202>
2020-04-04 16:14:33 +0900

    ユークリッドの互助法

--- Euclid.scala
+++ Euclid.scala
@@ -1,7 +1,11 @@
 object Euclid extends App {
 
   def greatestCommonDivisor(a: Int, b: Int): Int = {
-    ???
+    if (b==0){
+      return a
+    }else{
+      return greatestCommonDivisor(b,a%b)
+    }
   }
-
+  println(greatestCommonDivisor(24,36))
 }
