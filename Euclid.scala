8dcb1e97dff3ab45be886b1d90848ae3bc0db60c
yakata66 <ponyo4869yakata398@gmail.com>
2020-05-22 07:18:12 +0900

    implement gcd.

--- Euclid.scala
+++ Euclid.scala
@@ -1,7 +1,7 @@
-object Euclid extends App {
-
-  def greatestCommonDivisor(a: Int, b: Int): Int = {
-    ???
-  }
-
+object Euclid extends App
+{
+    def greatestCommonDivisor(a: Int, b:Int): Int = {
+        if (b==0) a
+        else greatestCommonDivisor(b, a%b)
+    }
 }
