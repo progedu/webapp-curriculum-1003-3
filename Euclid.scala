7159375dd6e89f9b4f9a97fd10b344ecbcac4bf4
saitoyoshi <mitti180@gmail.com>
2023-05-19 10:51:01 +0900

    ユークリッドの互除法を使い最大公約数を求める

--- Euclid.scala
+++ Euclid.scala
@@ -1,7 +1,14 @@
 object Euclid extends App {
 
-  def greatestCommonDivisor(a: Int, b: Int): Int = {
-    ???
-  }
+  val num1 = 36
+  val num2 = 24
 
+  def greatestCommonDivisor(a: Int, b: Int): Int = {
+    val r = a % b
+    if (r == 0) {
+      return b
+    }
+    greatestCommonDivisor(b, r)
+  }
+  println(greatestCommonDivisor(num1, num2))
 }
